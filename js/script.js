0dcc7415732626d5843dd2b8ef6ab46196f58a29
OUSee <666rld999@mail.ru>
2025-03-28 10:18:09 +0300

    adaptations

--- js/script.js
+++ js/script.js
@@ -11,18 +11,41 @@ const slideCount = document.querySelectorAll('.slide').length;
 const height = slide.offsetHeight;
 
 function updateSlider() {
-    slides.style.transform = `translateY(-${currentIndex * (height) + 2}px)`;
+    slides.style.transform = `translateY(-${currentIndex * (height) + 1}px)`;
 }
 
 nextBtn.addEventListener('click', () => {
-    currentIndex = (currentIndex + 1) % slideCount;
-    updateSlider();
+  clearInterval(autoScrollIntervalId); // Stop autoscroll on manual navigation
+  if (currentIndex < slideCount - 2) { // Move to next slide if not at the end
+      currentIndex++;
+  } else { // Roll back to the start if at the end
+      currentIndex = 0;
+  }
+  updateSlider();
 });
 
 prevBtn.addEventListener('click', () => {
-    currentIndex = (currentIndex - 1 + slideCount) % slideCount;
-    updateSlider();
+  clearInterval(autoScrollIntervalId); // Stop autoscroll on manual navigation
+  if (currentIndex > 0) { // Move to previous slide if not at the start
+      currentIndex--;
+  } else { // Roll back to the end if at the start
+      currentIndex = slideCount - 2; // Adjusted to show two slides at once
+  }
+  updateSlider();
 });
+
+function startAutoScroll() {
+  autoScrollIntervalId = setInterval(() => {
+    if (currentIndex < slideCount - 2) { // Move to next slide if not at the end
+      currentIndex++;
+    } else { // Roll back to the start if at the end
+        currentIndex = 0;
+    }
+  updateSlider();
+  }, 5000);
+}
+
+startAutoScroll();
 
 // opacity header onscroll
 
