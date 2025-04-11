d0f766b65eb15822b8ebd1254417f5837952c784
Kind Samurai <57537080+etozhejuicy@users.noreply.github.com>
2025-04-11 15:44:29 +0300

    create videoElement function & fix popup styles

--- js/script.js
+++ js/script.js
@@ -78,11 +78,9 @@ window.addEventListener('resize', () => {
 
 // SLIDER END
 
-
 // HORIZONTAL SLIDER
 
-const SliderInIt = () => {}
-
+const SliderInIt = () => {};
 
 document.addEventListener('DOMContentLoaded', () => {
   const sliders = document.querySelectorAll('.tab-slider');
@@ -90,151 +88,152 @@ document.addEventListener('DOMContentLoaded', () => {
   const tabButtons = document.querySelector('.slider-with-tabs_tabs-buttons');
   const tabButtonsList = tabButtons.querySelectorAll('label.button-link');
 
-
-  window.addEventListener('resize', ()=>{
-    if(window.innerWidth > 600){
+  window.addEventListener('resize', () => {
+    if (window.innerWidth > 600) {
       tabButtonsList.forEach((tabButton, index) => {
-        const input = tabButton.querySelector(`#tab-slide-btn-${index+1}`)
+        const input = tabButton.querySelector(`#tab-slide-btn-${index + 1}`);
         input.addEventListener('click', () => {
-          const activeSlider = document.querySelector(`#tab-slide-${index+1}`)
-          SliderHandler(activeSlider)
-        })
+          const activeSlider = document.querySelector(
+            `#tab-slide-${index + 1}`
+          );
+          SliderHandler(activeSlider);
+        });
       });
-  
-    
-    const SliderHandler = (slider) => {
+
+      const SliderHandler = (slider) => {
         const cards = slider.querySelectorAll('.tab-slider-card');
         const prevButton = document.querySelector(`.slider-arrow.prev`);
         const nextButton = document.querySelector(`.slider-arrow.next`);
-        
+
         let currentIndex = 0;
-  
+
         const updateSlider = () => {
           const sliderWidth = slider.offsetWidth;
           const slidesPerPage = Math.floor(sliderWidth / 255);
           const maxIndex = cards.length - slidesPerPage;
-          const cardWidth = Math.floor(sliderWidth / slidesPerPage -25) ;
-  
-          console.log('sliderWidth', sliderWidth)
-          console.log('cards', cards.length)
-          console.log('maxIndex', maxIndex)
-          console.log('slidesPerPage', slidesPerPage)
-          console.log('current index: ', currentIndex)
-  
-    
+          const cardWidth = Math.floor(sliderWidth / slidesPerPage - 25);
+
+          console.log('sliderWidth', sliderWidth);
+          console.log('cards', cards.length);
+          console.log('maxIndex', maxIndex);
+          console.log('slidesPerPage', slidesPerPage);
+          console.log('current index: ', currentIndex);
+
           cards.forEach((card) => {
             card.style.minWidth = `${cardWidth}px`;
-          })
-          
+          });
+
           slider.style.transform = `translateX(-${currentIndex * cardWidth}px)`;
-  
-          if(currentIndex === 0 || cards.length <= slidesPerPage){
+
+          if (currentIndex === 0 || cards.length <= slidesPerPage) {
             prevButton.style.display = 'none';
-          }
-          else{
+          } else {
             prevButton.style.display = 'block';
           }
-      
-          if(currentIndex === maxIndex || maxIndex <= 0 || cards.length <= slidesPerPage){
-            nextButton.style.display = 'none'
-          }
-          else{
+
+          if (
+            currentIndex === maxIndex ||
+            maxIndex <= 0 ||
+            cards.length <= slidesPerPage
+          ) {
+            nextButton.style.display = 'none';
+          } else {
             nextButton.style.display = 'block';
           }
         };
-  
+
         prevButton.addEventListener('click', () => {
-            currentIndex--;
-            updateSlider();
+          currentIndex--;
+          updateSlider();
         });
-  
+
         nextButton.addEventListener('click', () => {
           const sliderWidth = slider.offsetWidth;
           const slidesPerPage = Math.floor(sliderWidth / 255);
           const maxIndex = Math.max(0, cards.length - slidesPerPage);
-          const cardWidth = Math.floor(sliderWidth / slidesPerPage -25) ;
-  
+          const cardWidth = Math.floor(sliderWidth / slidesPerPage - 25);
+
           cards.forEach((card) => {
             card.style.minWidth = `${cardWidth}px`;
-          })
-  
+          });
+
           currentIndex++;
           updateSlider();
         });
-  
+
         window.addEventListener('resize', updateSlider);
         updateSlider();
-    }
-  
-    SliderHandler(document.querySelector(`#tab-slide-${1}`))
-    }
-  })
+      };
 
-  if(window.innerWidth > 600){
+      SliderHandler(document.querySelector(`#tab-slide-${1}`));
+    }
+  });
+
+  if (window.innerWidth > 600) {
     tabButtonsList.forEach((tabButton, index) => {
-      const input = tabButton.querySelector(`#tab-slide-btn-${index+1}`)
+      const input = tabButton.querySelector(`#tab-slide-btn-${index + 1}`);
       input.addEventListener('click', () => {
-        const activeSlider = document.querySelector(`#tab-slide-${index+1}`)
-        SliderHandler(activeSlider)
-      })
+        const activeSlider = document.querySelector(`#tab-slide-${index + 1}`);
+        SliderHandler(activeSlider);
+      });
     });
 
-  
-  const SliderHandler = (slider) => {
+    const SliderHandler = (slider) => {
       const cards = slider.querySelectorAll('.tab-slider-card');
       const prevButton = document.querySelector(`.slider-arrow.prev`);
       const nextButton = document.querySelector(`.slider-arrow.next`);
-      
+
       let currentIndex = 0;
 
       const updateSlider = () => {
         const sliderWidth = slider.offsetWidth;
         const slidesPerPage = Math.floor(sliderWidth / 255);
         const maxIndex = cards.length - slidesPerPage;
-        const cardWidth = Math.floor(sliderWidth / slidesPerPage -25) ;
+        const cardWidth = Math.floor(sliderWidth / slidesPerPage - 25);
 
-        console.log('sliderWidth', sliderWidth)
-        console.log('cards', cards.length)
-        console.log('maxIndex', maxIndex)
-        console.log('slidesPerPage', slidesPerPage)
-        console.log('current index: ', currentIndex)
+        console.log('sliderWidth', sliderWidth);
+        console.log('cards', cards.length);
+        console.log('maxIndex', maxIndex);
+        console.log('slidesPerPage', slidesPerPage);
+        console.log('current index: ', currentIndex);
 
-  
         cards.forEach((card) => {
           card.style.minWidth = `${cardWidth}px`;
-        })
-        
+        });
+
         slider.style.transform = `translateX(-${currentIndex * cardWidth}px)`;
 
-        if(currentIndex === 0 || cards.length <= slidesPerPage){
+        if (currentIndex === 0 || cards.length <= slidesPerPage) {
           prevButton.style.display = 'none';
-        }
-        else{
+        } else {
           prevButton.style.display = 'block';
         }
-    
-        if(currentIndex === maxIndex || maxIndex <= 0 || cards.length <= slidesPerPage){
-          nextButton.style.display = 'none'
-        }
-        else{
+
+        if (
+          currentIndex === maxIndex ||
+          maxIndex <= 0 ||
+          cards.length <= slidesPerPage
+        ) {
+          nextButton.style.display = 'none';
+        } else {
           nextButton.style.display = 'block';
         }
       };
 
       prevButton.addEventListener('click', () => {
-          currentIndex--;
-          updateSlider();
+        currentIndex--;
+        updateSlider();
       });
 
       nextButton.addEventListener('click', () => {
         const sliderWidth = slider.offsetWidth;
         const slidesPerPage = Math.floor(sliderWidth / 255);
         const maxIndex = Math.max(0, cards.length - slidesPerPage);
-        const cardWidth = Math.floor(sliderWidth / slidesPerPage -25) ;
+        const cardWidth = Math.floor(sliderWidth / slidesPerPage - 25);
 
         cards.forEach((card) => {
           card.style.minWidth = `${cardWidth}px`;
-        })
+        });
 
         currentIndex++;
         updateSlider();
@@ -242,13 +241,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
       window.addEventListener('resize', updateSlider);
       updateSlider();
+    };
+
+    SliderHandler(document.querySelector(`#tab-slide-${1}`));
   }
-
-  SliderHandler(document.querySelector(`#tab-slide-${1}`))
-  }
-
-  });
-
+});
 
 // opacity header onscroll
 
@@ -263,8 +260,9 @@ window.addEventListener('scroll', function () {
 
 // popup logic
 
-const popupTriggers = document.querySelectorAll('.popup-trigger');
-console.log(popupTriggers[0].dataset.popup);
+// const popupTriggers = document.querySelectorAll('.popup-trigger');
+// console.log(popupTriggers[0].dataset.popup);
+const popupTriggers = document.querySelectorAll('[data-popup]');
 popupTriggers.forEach((trigger) => {
   trigger.addEventListener('click', () => {
     const popupId = trigger.dataset.popup;
@@ -272,20 +270,43 @@ popupTriggers.forEach((trigger) => {
   });
 });
 
+// Специально, для видео из кружочка
+const toggleVideoPLay = (videoElement, init) => {
+  if (videoElement) {
+    init === true ? videoElement.play() : videoElement.pause();
+    init !== true ? (videoElement.currentTime = 0) : false;
+    videoElement.setAttribute('playinline', init === true ? true : false);
+    videoElement.autoplay = init === true ? true : false;
+    videoElement.controls = init === true ? true : false;
+    videoElement.loop = init === true ? true : false;
+    videoElement.muted = init === true ? false : true;
+  }
+};
+
 const openPopup = (id) => {
-  console.log('open ', id);
+  // console.log('open ', id);
   const popup = document.getElementById(id);
-  console.log(popup);
+  let videoPopup = popup.querySelector('video');
+  // console.log(popup);
   popup.classList.add('open');
+
+  // Приостанавливаем прокрутку страницы, когда окно открыто
+  document.documentElement.classList.add('popup-opened');
+  videoPopup ? toggleVideoPLay(videoPopup, true) : false;
 
   popup.addEventListener('click', (e) => {
     e.stopPropagation();
     e.preventDefault();
-    console.log('dialog -', e.target);
+    // console.log('dialog -', e.target);
     if (e.target.classList.contains('close-button') || e.target.id == id) {
+      let videoPopup = popup.querySelector('video');
+
       popup.classList.remove('open');
+
+      // Возвращаем прокрутку страницы, когда окно закрыто
+      document.documentElement.classList.remove('popup-opened');
+
+      videoPopup ? toggleVideoPLay(videoPopup, false) : false;
     }
   });
 };
-
-console.log(1);
