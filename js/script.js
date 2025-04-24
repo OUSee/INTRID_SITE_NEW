a93e42c1bc73fbb513d289e1aeeb7f416768501e
OUSee <666rld999@mail.ru>
2025-04-24 11:29:25 +0300

    almost ready

--- js/script.js
+++ js/script.js
@@ -381,8 +381,8 @@ const openPopup = (id) => {
   videoPopup ? toggleVideoPLay(videoPopup, true) : false;
 
   popup.addEventListener('click', (e) => {
-    e.stopPropagation();
-    e.preventDefault();
+    // e.stopPropagation();
+    // e.preventDefault();
     if (e.target.classList.contains('close-button') || e.target.id == id) {
       let videoPopup = popup.querySelector('video');
 
@@ -512,3 +512,33 @@ const setTenderTabContent = (tabId) => {
 }
 
 setTenderTabContent('tender-radio-1')
+
+// MAP LOGIC
+
+const mapLinks = document.querySelectorAll('.map-link')
+console.log('=> maplinks', mapLinks)
+
+const mapData = {
+  prague: {frame: '<iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3A596c110a6e67b7ecf53baf573cb97950916b7bfb81fe164b7ec895af9864ccb8&amp;source=constructor" width="600" height="400" frameborder="0"></iframe>', link: 'https://yandex.com/maps/-/CHfqQD44'},
+  vrn: {frame: '<iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3Ade02f3d1f93ca59ddbc7669962c5e7f350cec0968beae8e096d86287b227d6b1&amp;source=constructor" width="500" height="400" frameborder="0"></iframe>',  link: 'https://yandex.com/maps/-/CHfmvO6X'}
+}
+
+mapLinks.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    console.log('=> dataset', e?.target?.dataset.map)
+    loadMap(e, e?.target?.dataset.map);
+  });
+});
+
+function loadMap(e, value) {
+  e.preventDefault();
+  const map = document.querySelector('#map');
+  console.log('=> map', map)
+  const container = map.querySelector('.map-container')
+  const linkBtn = map.querySelector('.button-link')
+  console.log('=> data', mapData[value])
+  console.log('=> value', value)
+  container.innerHTML = mapData[value].frame
+  linkBtn.href = mapData[value].link
+
+}
