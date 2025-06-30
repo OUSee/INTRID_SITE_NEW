ab5f353e615889445941c1b11fe05b379da910b3
OUSee <666rld999@mail.ru>
2025-06-30 16:56:13 +0300

    fixes

--- js/map-handler.js
+++ js/map-handler.js
@@ -6,8 +6,8 @@ document.addEventListener('DOMContentLoaded', () => {
     // console.log('=> links found', mapLinks)
 
     const mapData = {
-      prague: {frame: '<iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3A596c110a6e67b7ecf53baf573cb97950916b7bfb81fe164b7ec895af9864ccb8&amp;source=constructor" width="835" height="516" frameborder="0"></iframe>', link: 'https://yandex.ru/maps/-/CHvuzWlD', adress: 'Poděbradská 52, 19000 Praha 9' },
-      vrn: {frame: '<iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3Ade02f3d1f93ca59ddbc7669962c5e7f350cec0968beae8e096d86287b227d6b1&amp;source=constructor" width="674" height="534" frameborder="0"></iframe>',  link: 'https://yandex.com/maps/-/CHfmvO6X', adress: 'г. Воронеж ул. Пятницкого, 40', schedule: 'Пн-Пт: 9:00-18:00'},
+      prague: {frame: '<iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3A596c110a6e67b7ecf53baf573cb97950916b7bfb81fe164b7ec895af9864ccb8&amp;source=constructor" width="835" height="516" frameborder="0"></iframe>', link: 'https://yandex.ru/maps/-/CHvuzWlD', adress: 'Poděbradská 52, 19000 Praha 9', phone: '420777749719', wa: '79518587172', tg: 'lionvdv'},
+      vrn: {frame: '<iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3Ade02f3d1f93ca59ddbc7669962c5e7f350cec0968beae8e096d86287b227d6b1&amp;source=constructor" width="674" height="534" frameborder="0"></iframe>',  link: 'https://yandex.com/maps/-/CHfmvO6X', adress: 'г. Воронеж ул. Пятницкого, 40', schedule: 'Пн-Пт: 9:00-18:00',  phone: '74732540796', wa: '74732540796', tg: 'webintrid'},
     }
   
     mapLinks.forEach((link) => {
