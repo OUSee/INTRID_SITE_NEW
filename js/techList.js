d7488c3c881fce26714d526e8a352854147f4fdf
Kind Samurai <57537080+etozhejuicy@users.noreply.github.com>
2025-07-08 12:36:22 +0300

    update techList()

--- js/techList.js
+++ js/techList.js
@@ -1,11 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
-  let lists = document.querySelectorAll('.tech-we-use-list');
+  let lists = document.querySelectorAll('.tech-we-use-list'),
+    interval = 0.6;
 
   lists.forEach((list) => {
     let items = list.querySelectorAll('li');
 
     if (list) {
-      list.style = `--list-length: ${items.length}`;
+      list.style.setProperty('--list-length', items.length);
+
+      items.forEach((item, index) => {
+        console.log(index);
+        item.querySelector('.tech-tag').style.animation = `list-glow ${
+          items.length * interval
+        }s linear infinite`;
+        item.querySelector('.tech-tag').style.animationDelay = `${
+          index * interval
+        }s`;
+      });
     }
   });
 });
