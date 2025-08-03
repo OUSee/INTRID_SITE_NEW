2755126c3b7b8a9612579581cfbc6db2a8d284cc
Kind Samurai <zenitovic@gmail.com>
2025-08-03 23:46:32 +0300

    update tabs

--- js/slider.js
+++ js/slider.js
@@ -296,15 +296,3 @@ document.addEventListener('DOMContentLoaded', () => {
     SliderHandler(document.querySelector(`#tab-slide-${1}`));
   }
 });
-
-
-// tabs init
-document.querySelectorAll('.prices-block--buttons input[type="radio"]').forEach(radio => {
-  radio.addEventListener('change', function() {
-    const tabId = this.id.replace('btn-', '');
-    document.querySelectorAll('[id^="tab-slide-"]').forEach(tab => {
-      tab.classList.remove('active');
-    });
-    document.getElementById(tabId).classList.add('active');
-  });
-});
