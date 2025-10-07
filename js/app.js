7c2ff890a9b29b78a91b8051a5a4620e2b2f6ae1
Kind Samurai <57537080+etozhejuicy@users.noreply.github.com>
2025-10-07 11:02:38 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -184,94 +184,91 @@ function isEmailValid(value) {
 }
 
 // popup logic
-document.addEventListener("DOMContentLoaded", () => {
-	const popupTriggers = document?.querySelectorAll("[data-popup]");
+const popupTriggers = document?.querySelectorAll("[data-popup]");
 
-	popupTriggers.forEach((trigger) => {
-		trigger.addEventListener("click", () => {
-			const popupId = trigger.dataset.popup;
-			openPopup(popupId);
-		});
+popupTriggers.forEach((trigger) => {
+	trigger.addEventListener("click", () => {
+		const popupId = trigger.dataset.popup;
+		openPopup(popupId);
 	});
+});
 
-	// Специально, для видео из кружочка
-	const toggleVideoPLay = (videoElement, init) => {
-		if (videoElement) {
-			init === true ? videoElement.play() : videoElement.pause();
-			videoElement.setAttribute(
-				"playinline",
-				init === true ? true : false
-			);
-			videoElement.autoplay = init === true ? true : false;
-			videoElement.controls = init === true ? true : false;
-			videoElement.loop = init === true ? true : false;
-			videoElement.muted = init === true ? false : true;
+// Специально, для видео из кружочка
+const toggleVideoPLay = (videoElement, init) => {
+	if (videoElement) {
+		init === true ? videoElement.play() : videoElement.pause();
+		videoElement.setAttribute("playinline", init === true ? true : false);
+		videoElement.autoplay = init === true ? true : false;
+		videoElement.controls = init === true ? true : false;
+		videoElement.loop = init === true ? true : false;
+		videoElement.muted = init === true ? false : true;
 
-			if (init === false) {
-				videoElement.currentTime = 0;
-			}
+		if (init === false) {
+			videoElement.currentTime = 0;
 		}
-	};
+	}
+};
 
-	const openPopup = (id) => {
-		// console.log('open ', id);
-		const popup = document.getElementById(id);
-		const onLoad = popup.dataset.onload;
+const openPopup = (id) => {
+	// console.log('open ', id);
+	const popup = document.getElementById(id);
+	const onLoad = popup.dataset.onload;
 
-		let buttonClose = document.createElement("button");
+	let buttonClose;
 
+	if (!popup.querySelector(".popup-close")) {
+		buttonClose = document.createElement("button");
 		buttonClose.classList.add("popup-close");
 		buttonClose.setAttribute("data-close-popup", true);
 		buttonClose.setAttribute("aria-label", "close-popup");
+	}
 
-		let videoPopup = popup.querySelector("video");
-		// console.log(popup);
+	let videoPopup = popup.querySelector("video");
+	// console.log(popup);
 
-		popup.classList.add("open");
+	popup.classList.add("open");
 
-		popup.querySelector(".popup-wrapper")
-			? popup?.querySelector(".popup-wrapper").prepend(buttonClose)
-			: popup.prepend(buttonClose);
+	popup.querySelector(".popup-wrapper")
+		? popup?.querySelector(".popup-wrapper").prepend(buttonClose)
+		: popup.prepend(buttonClose);
 
-		// Приостанавливаем прокрутку страницы, когда окно открыто
-		document.documentElement.classList.add("popup-opened");
-		videoPopup ? toggleVideoPLay(videoPopup, true) : false;
+	// Приостанавливаем прокрутку страницы, когда окно открыто
+	document.documentElement.classList.add("popup-opened");
+	videoPopup ? toggleVideoPLay(videoPopup, true) : false;
 
-		if (onLoad) {
-			window[onLoad]();
-		}
+	if (onLoad) {
+		window[onLoad]();
+	}
 
-		popup.addEventListener("click", (e) => {
-			if (
-				e.target.classList.contains("popup-close") ||
-				e.target.dataset.closePopup ||
-				e.target.classList.contains("popup-wrapper") ||
-				e.target.id == id
-			) {
-				if (
-					e.target.tagName &&
-					e.target.tagName.toLowerCase() !== "a"
-				) {
-					e.stopPropagation();
-					e.preventDefault();
-				}
-
-				let videoPopup = popup.querySelector("video");
-				let iframe = popup.querySelector("iframe");
-
-				popup.classList.remove("open");
-
-				buttonClose.remove();
-
-				// Возвращаем прокрутку страницы, когда окно закрыто
-				document.documentElement.classList.remove("popup-opened");
-
-				videoPopup ? toggleVideoPLay(videoPopup, false) : false;
-				iframe ? iframe.remove() : false;
+	popup.addEventListener("click", (e) => {
+		if (
+			e.target.classList.contains("popup-close") ||
+			e.target.dataset.closePopup ||
+			e.target.classList.contains("popup-wrapper") ||
+			e.target.id == id
+		) {
+			if (e.target.tagName && e.target.tagName.toLowerCase() !== "a") {
+				e.stopPropagation();
+				e.preventDefault();
 			}
-		});
-	};
-});
+
+			let videoPopup = popup.querySelector("video");
+			let iframe = popup.querySelector("iframe");
+
+			popup.classList.remove("open");
+
+			if (buttonClose) {
+				buttonClose.remove();
+			}
+
+			// Возвращаем прокрутку страницы, когда окно закрыто
+			document.documentElement.classList.remove("popup-opened");
+
+			videoPopup ? toggleVideoPLay(videoPopup, false) : false;
+			iframe ? iframe.remove() : false;
+		}
+	});
+};
 
 // MAP LOGIC
 document.addEventListener("DOMContentLoaded", () => {
@@ -401,6 +398,28 @@ document.addEventListener("DOMContentLoaded", () => {
 
 		if (!slidesContainer || !originalSlides.length) {
 			return;
+		}
+
+		// Делегирование событий для кликов по слайдам
+		slidesContainer.addEventListener("click", (e) => {
+			const slide = e.target.closest(".slide");
+			if (slide) {
+				// Получаем индекс оригинального слайда
+				const allSlides = slidesContainer.querySelectorAll(".slide");
+				const slideIndex = Array.from(allSlides).indexOf(slide);
+				const realIndex =
+					(slideIndex - 2 + originalSlides.length) %
+					originalSlides.length;
+
+				// Вызываем обработчик события для слайда
+				handleSlideClick(realIndex, slide);
+			}
+		});
+
+		// Функция для обработки кликов по слайдам
+		function handleSlideClick(index, slideElement) {
+			// console.log("Clicked slide:", index);
+			index === 0 ? openPopup(slideElement.dataset.popup) : "";
 		}
 
 		// Создаем клоны для плавных переходов
