90e2b2d90be73a975b41382b8d3edca304c74660
Kind Samurai <57537080+etozhejuicy@users.noreply.github.com>
2025-12-29 09:13:46 +0300

    fix & photoViewer

--- js/app.js
+++ js/app.js
@@ -605,8 +605,8 @@ document.addEventListener("DOMContentLoaded", () => {
 			.catch((error) => {
 				const message =
 					error &&
-						typeof error.message === "string" &&
-						error.message.trim() !== ""
+					typeof error.message === "string" &&
+					error.message.trim() !== ""
 						? error.message
 						: defaultErrorMessage;
 				showNotificationPopup(message, "error");
@@ -660,7 +660,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 				const sitekey =
 					item.recaptchaContainer.dataset.sitekey &&
-						item.recaptchaContainer.dataset.sitekey.length > 0
+					item.recaptchaContainer.dataset.sitekey.length > 0
 						? item.recaptchaContainer.dataset.sitekey
 						: defaultSiteKey;
 
@@ -766,8 +766,8 @@ document.addEventListener("DOMContentLoaded", () => {
 				mapData[value].adress +
 				(mapData[value]?.schedule
 					? '<span class="separator">|</span><span> ' +
-					mapData[value].schedule +
-					'</span><span class="separator">|</span>'
+					  mapData[value].schedule +
+					  '</span><span class="separator">|</span>'
 					: '<span class="separator">|</span>');
 			container.innerHTML = mapData[value].frame;
 
@@ -834,10 +834,12 @@ document.addEventListener("DOMContentLoaded", () => {
 			list.style.setProperty("--list-length", items.length);
 
 			items.forEach((item, index) => {
-				item.querySelector(".tech-tag").style.animation = `list-glow ${items.length * interval
-					}s linear infinite`;
-				item.querySelector(".tech-tag").style.animationDelay = `${index * interval
-					}s`;
+				item.querySelector(".tech-tag").style.animation = `list-glow ${
+					items.length * interval
+				}s linear infinite`;
+				item.querySelector(".tech-tag").style.animationDelay = `${
+					index * interval
+				}s`;
 			});
 		}
 	});
@@ -1378,8 +1380,8 @@ const SliderInIt = () => {
 							: "block";
 					nextButton.style.display =
 						currentIndex === maxIndex ||
-							maxIndex <= 0 ||
-							cards.length <= slidesPerPage
+						maxIndex <= 0 ||
+						cards.length <= slidesPerPage
 							? "none"
 							: "block";
 
@@ -2041,24 +2043,32 @@ function sliderInitialize() {
 
 				prevBtn.forEach((btn) => {
 					if (slider.id === "gallery-slider") {
-						btn.style = `position: absolute; transform: translateY(-${sliderHeight}px) translateX(${window.innerWidth > 600 ? `-100%` : `0`
-							}); left: ${window.innerWidth > 600 ? `25px` : `-5px`
-							}; color: var(--blue-main);`;
+						btn.style = `position: absolute; transform: translateY(-${sliderHeight}px) translateX(${
+							window.innerWidth > 600 ? `-100%` : `0`
+						}); left: ${
+							window.innerWidth > 600 ? `25px` : `-5px`
+						}; color: var(--blue-main);`;
 					} else {
-						btn.style = `position: absolute; transform: translateY(-${sliderHeight}px) translateX(${window.innerWidth > 600 ? `-100%` : `0`
-							}); left: ${window.innerWidth > 600 ? `15px` : `-5px`
-							}; color: var(--blue-main);`;
+						btn.style = `position: absolute; transform: translateY(-${sliderHeight}px) translateX(${
+							window.innerWidth > 600 ? `-100%` : `0`
+						}); left: ${
+							window.innerWidth > 600 ? `15px` : `-5px`
+						}; color: var(--blue-main);`;
 					}
 				});
 				nextBtn.forEach((btn) => {
 					if (slider.id === "gallery-slider") {
-						btn.style = `position: absolute; transform: translateY(-${sliderHeight}px) translateX(${window.innerWidth > 600 ? `100%` : `0`
-							}); right: ${window.innerWidth > 600 ? `25px` : `-5px`
-							}; color: var(--blue-main);`;
+						btn.style = `position: absolute; transform: translateY(-${sliderHeight}px) translateX(${
+							window.innerWidth > 600 ? `100%` : `0`
+						}); right: ${
+							window.innerWidth > 600 ? `25px` : `-5px`
+						}; color: var(--blue-main);`;
 					} else {
-						btn.style = `position: absolute; transform: translateY(-${sliderHeight}px) translateX(${window.innerWidth > 600 ? `100%` : `0`
-							}); right: ${window.innerWidth > 600 ? `15px` : `-5px`
-							}; color: var(--blue-main);`;
+						btn.style = `position: absolute; transform: translateY(-${sliderHeight}px) translateX(${
+							window.innerWidth > 600 ? `100%` : `0`
+						}); right: ${
+							window.innerWidth > 600 ? `15px` : `-5px`
+						}; color: var(--blue-main);`;
 					}
 				});
 			};
@@ -3431,7 +3441,7 @@ document.addEventListener("DOMContentLoaded", () => {
 					}
 					if (
 						counter.elementref.dataset?.intendfor ===
-						"design-landing" &&
+							"design-landing" &&
 						counter.elementref.value < 1
 					) {
 						counter.elementref.value = 1;
@@ -4685,3 +4695,22 @@ if (typeof module !== "undefined" && module.exports) {
 
 // Собственный root-margin observer
 // data-root-margin="-15% 0px -65% 0px"
+
+// office-viewer
+const officeImage = (elem) => {
+	let link = elem.querySelector("img").getAttribute("src"),
+		modal = document.getElementById("photo-view"),
+		img = modal?.querySelector("img");
+
+	img.setAttribute("src", link);
+};
+
+let photoCards = document?.querySelectorAll(".card--photo");
+
+if (photoCards.length > 0) {
+	photoCards.forEach((item) => {
+		item.addEventListener("click", (e) => {
+			officeImage(e.currentTarget);
+		});
+	});
+}
