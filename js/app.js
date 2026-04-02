1144e490b1c8968ca9aac4754e788eb219184bed
Kind Samurai <zenitovic@gmail.com>
2026-04-02 14:45:59 +0300

    update mask phone function

--- js/app.js
+++ js/app.js
@@ -2135,86 +2135,46 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 // phone field handler
-document.addEventListener("DOMContentLoaded", () => {
-	const fields = document.querySelectorAll("input[type=tel]");
-	const phonePattern =
-		"^(7|8)[\\s\\-]?\\(?\\d{3}\\)?[\\s\\-]?\\d{3}[\\s\\-]?\\d{2}[\\s\\-]?\\d{2}$";
+function maskPhone(selector, masked = '+7 (___) ___-__-__') {
+	const elems = document.querySelectorAll(selector);
 
-	fields.forEach((field) => {
-		field.addEventListener("beforeinput", (e) => {
-			const input = e.target;
-			const value = input.value;
-			const digits = value.replace(/\D/g, "");
-			const isDigit = /\d/.test(e.data);
+	function mask(event) {
+		const keyCode = event.keyCode;
+		const template = masked,
+			def = template.replace(/\D/g, ""),
+			val = this.value.replace(/\D/g, "");
+		console.log(template);
+		let i = 0,
+			newValue = template.replace(/[_\d]/g, function (a) {
+				return i < val.length ? val.charAt(i++) || def.charAt(i) : a;
+			});
+		i = newValue.indexOf("_");
+		if (i !== -1) {
+			newValue = newValue.slice(0, i);
+		}
+		let reg = template.substr(0, this.value.length).replace(/_+/g,
+			function (a) {
+				return "\\d{1," + a.length + "}";
+			}).replace(/[+()]/g, "\\$&");
+		reg = new RegExp("^" + reg + "$");
+		if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) {
+			this.value = newValue;
+		}
+		if (event.type === "blur" && this.value.length < 5) {
+			this.value = "";
+		}
 
-			if (isDigit && digits.length >= 11) {
-				e.preventDefault();
-			}
-		});
+	}
 
-		field.addEventListener("input", (e) => {
-			const input = e.target;
-			field.setAttribute("pattern", phonePattern);
+	for (const elem of elems) {
+		elem.addEventListener("input", mask);
+		elem.addEventListener("focus", mask);
+		elem.addEventListener("blur", mask);
+	}
 
-			let cursorPos = input.selectionStart;
-			const oldValue = input.value;
+}
 
-			let digits = oldValue.replace(/\D/g, "");
-
-			if (digits.length > 0 && digits[0] !== "7") {
-				digits = "7" + digits.slice(1);
-			}
-
-			if (digits.length > 11) {
-				digits = digits.slice(0, 11);
-			}
-
-			function formatPhone(digits) {
-				let formatted = "";
-				if (digits.length > 0) {
-					formatted += digits[0];
-				}
-				if (digits.length >= 2) {
-					formatted += " (" + digits.slice(1, 4);
-				}
-				if (digits.length >= 5) {
-					formatted += ") " + digits.slice(4, 7);
-				}
-				if (digits.length >= 8) {
-					formatted += " " + digits.slice(7, 9);
-				}
-				if (digits.length >= 10) {
-					formatted += " " + digits.slice(9, 11);
-				}
-				return formatted;
-			}
-
-			const formatted = formatPhone(digits);
-
-			function countDigits(str, pos) {
-				return (str.slice(0, pos).match(/\d/g) || []).length;
-			}
-
-			const digitsBeforeCursor = countDigits(oldValue, cursorPos);
-
-			let newCursorPos = 0;
-			let digitsCount = 0;
-
-			while (
-				digitsCount < digitsBeforeCursor &&
-				newCursorPos < formatted.length
-			) {
-				if (/\d/.test(formatted[newCursorPos])) {
-					digitsCount++;
-				}
-				newCursorPos++;
-			}
-
-			input.value = formatted;
-			input.setSelectionRange(newCursorPos, newCursorPos);
-		});
-	});
-});
+maskPhone('input[type="tel"]', '+7 (___) ___-__-__');
 
 // textarea autosize
 window.textarea_autosize = {
